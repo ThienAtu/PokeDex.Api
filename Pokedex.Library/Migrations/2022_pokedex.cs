0f3252608e1e99dc2afac5ee8a9cd023b8167ecb
beesama197@gmail.com <beesama197@gmail.com>
2022-12-31 12:31:03 +0700

    Custom EF (20223112)

---
+++ Pokedex.Library/Migrations/2022_pokedex.cs
@@ -0,0 +1,9 @@
+﻿using Pokedex.Library.Migrations.Services;
+
+namespace Pokedex.Library.Migrations;
+
+public partial class Pokedex : MigrationsExecute
+{
+	public Pokedex()
+		: base("pokedex") { }
+}
---
+++ Pokedex.Library/Migrations/Services/MigrationsExecute.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Pokedex.Library.Migrations.Services;
+
+public abstract class MigrationsExecute : Migration
+{
+	private readonly string _sqlFile;
+
+	public MigrationsExecute(string sqlFile)
+	{
+		_sqlFile = sqlFile;
+	}
+
+	protected override void Up(MigrationBuilder migrationBuilder)
+	{
+		RunSqlQuery(migrationBuilder, "up");
+	}
+
+	protected override void Down(MigrationBuilder migrationBuilder)
+	{
+		RunSqlQuery(migrationBuilder, "down");
+	}
+
+	private void RunSqlQuery(MigrationBuilder migrationBuilder, string action)
+	{
+		var file = Path.GetFullPath($"Migrations/Scripts/{_sqlFile}_{action}.sql");
+		var reader = new StreamReader(file);
+		var data = reader.ReadToEnd();
+		migrationBuilder.Sql(data);
+	}
+}
